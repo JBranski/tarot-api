0aefcf3d410a5ba75e5f6372d1721483fcfa10c8
Faith <jonathan.branski96@gmail.com>
2023-05-11 10:40:44 -0500

    added three tarot spread

--- index.js
+++ index.js
@@ -1,28 +1,36 @@
 const CARDS = require('./tarot.json');
 
-function pickTarot(cards) {
-    let orientation;
-    let cardNum;
-    let card;
-    let desc;
+function threeTarotSpread(cards) {
+    let spread = [];
     let str;
 
-    // pick orientation
-    if(Math.round(Math.random()) > 0) {
-        orientation = "upright";
-    } else {
-        orientation = "reversed";
+    for (let i = 0; i < 3; i++) {
+        let num, card, orientation, desc;
+
+        num = Math.round(Math.random() * cards.tarot.length - 1);
+        card = cards.tarot[num].title;
+
+        if(Math.round(Math.random()) == 0) {
+            oriNum = 0;
+            orientation = "upright";
+        } else {
+            oriNum = 1;
+            orientation = "reversed";
+        }
+
+        if(oriNum == 0) {
+            desc = cards.tarot[num].orientation[oriNum].upright;
+        } else {
+            desc = cards.tarot[num].orientation[oriNum].reversed;
+        }
+
+        spread.push([card, orientation, desc]);        
     }
 
-    // pick card
-    cardNum = Math.round(Math.random() * cards.tarot.length - 1);
-    card = cards.tarot[cardNum].title;
-    desc = cards.tarot[cardNum].desc;
+    str = `The Fates have answered your call. Your first card is ${spread[0][0]} - ${spread[0][1]} (${spread[0][2]}). Your second card is ${spread[1][0]} - ${spread[1][1]} (${spread[1][2]}). And your final card is ${spread[2][0]} - ${spread[2][1]} (${spread[2][2]})!`
 
-    str = `Fates have drawn you to ${card}! ${desc}`;
-
+    // console.log(str)
     return str;
-
 }
 
 // Add Express
@@ -33,9 +41,13 @@ const app = express();
 
 // Create GET request
 app.get("/", (req, res) => {
-    res.send(
-        pickTarot(CARDS)
-    );
+    try {
+        res.send(
+            threeTarotSpread(CARDS)
+        );
+    } catch (error) {
+        res.status(500).send("Oops, an error occurred — please try again shortly!");
+    }
 });
 
 // Initialize server
